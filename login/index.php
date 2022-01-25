f33f3263991a5c97be05372291977638a29a0d69
Xou <73950455+Xou7777@users.noreply.github.com>
2022-01-25 09:52:53 +0100

    *

--- login/index.php
+++ login/index.php
@@ -1,1 +1,1 @@
-<?php header("Location: https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_WEBSITE_URL&response_type=code&scope=identify");?>
+<?php header("Location: https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_WEBSITE_URL/callback/&response_type=code&scope=identify");?>
